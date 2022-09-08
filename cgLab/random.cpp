38ad8eecac556900b520b42daf158755412636c9
krahul2024 <76573313+krahul2024@users.noreply.github.com>
2022-09-08 08:21:27 +0530

    Create random.cpp

---
+++ cgLab/random.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  // for cicle
+    // int r=10,p=1-r,x=0,y=r,t;
+    // while(x<y){
+    //   cout<<p<<" ";
+    //   t=p+2*x+3;
+    //   x++;
+    //   if(p>=0)y--,t-=2*y;
+    //   p=t;
+    //   
+    // }
+    // for ellipse
+    // int a=8,b=6,x=1,y=b,t,p=b*b+(a*a)/4-a*a*b,dx=b*b*x,dy=a*a*y;
+    // while(dx<dy){
+    //   cout<<p<<" ";
+    //   x++;
+    //   t=p+b*b*(2*x+1);
+    //   if(p>=0)y--,t-=a*a*2*y;
+    //   p=t;
+    //   cout<<x<<" "<<y<<"\n";
+    //   dx=b*b*x,dy=a*a*y;
+    // }
+    // cout<<"\n";
+    // p=-151;
+    // while(y>0){
+    //   cout<<p<<" ";
+    //   y--;
+    //   t=p+a*a*(1-2*y);
+    //   if(p<=0)x++,t+=b*b*2*x;
+    //   p=t;
+    //   cout<<x<<" "<<y<<"\n";
+    // }
+    
+    
+}
