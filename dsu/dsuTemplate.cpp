aea21253119867f20cf198752c7a0ddadfe1bbb6
krahul2024 <76573313+krahul2024@users.noreply.github.com>
2022-09-13 00:16:24 +0530

    Create dsuTemplate.cpp

---
+++ dsu/dsuTemplate.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=2e5+111,mod=1e9+7;
+#define pb push_back
+#define all(x) begin(x),end(x)
+vector<int>parent(N),size(N,1);
+void makeSet(int x){
+  parent[x]=x;
+}
+int findSet(int x){
+  if(x==parent[x]) return x;
+  return parent[x]=findSet(parent[x]);
+}
+void unionSet(int x,int y){
+  x=findSet(x),y=findSet(y);
+  if(x!=y){
+    // in this operation we check size of which set is greater and assign parent of greater
+    // set as parent of smaller set which makes union of these two sets hence the swapping.
+    if(size[x]<size[y])swap(x,y);
+    parent[y]=x;
+    size[x]+=size[y];
+  }
+  return;
+}
